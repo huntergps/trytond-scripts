4bc6bf449a0f15ffab2612b13fe8a6095ca31ddb
Kulv3r <kulver@gmail.com>
2016-05-20 16:50:24 +0300

    refined Dockerfile, modules now installed from pip

---
+++ download_all_modules_pip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Downloading modules of version "$1
+
+target_version=$1
+target_version_next=$(awk "BEGIN {print $target_version+0.1}")
+
+echo 'Downloading a list of all the modules...'
+wget https://downloads.tryton.org/modules.txt
+
+for i in $( cat modules.txt )
+do
+    pip install "trytond_$i>=$target_version.0,<$target_version_next.0"
+done
+
+echo 'Cleaning modules.txt file...'
+rm modules.txt
