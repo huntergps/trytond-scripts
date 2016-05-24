eac4bb6bdf84fc49f895573af4f5a44880d55551
Kulv3r <kulver@gmail.com>
2016-05-24 18:54:45 +0300

    docker entry script tests

--- docker_entry_script.sh
+++ docker_entry_script.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+echo "Generating config..."
 python /tryton/trytond-scripts/generate_config.py
 
-exec /usr/local/bin/trytond "$@"
+echo "Generating password file..."
+echo "$TRYTONPASS" > $TRYTONPASSFILE
+
+echo "Initializing the DB..."
+/usr/local/bin/trytond -c $TRYTOND_CONFIG -d $DATABASE_NAME -v --all
+
+echo "Removing password file..."
+rm $TRYTONPASSFILE
+
+#echo "Launching Trytond Server..."
+#exec /usr/local/bin/trytond -c $TRYTOND_CONFIG -d $DATABASE_NAME -v
