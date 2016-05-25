df6ad5b7b8b77f077ca44f4f9853f952f33076e6
Kulv3r <kulver@gmail.com>
2016-05-26 00:03:11 +0300

    v4.0

--- docker_entry_script.sh
+++ docker_entry_script.sh
@@ -9,7 +9,7 @@ echo "Generating password file..."
 echo "$TRYTONPASS" > $TRYTONPASSFILE
 
 echo "Initializing the DB..."
-/usr/local/bin/trytond -c $TRYTOND_CONFIG -d $DATABASE_NAME -v --all
+trytond-admin -c $TRYTOND_CONFIG -d $DATABASE_NAME -v --all
 
 echo "Removing password file..."
 rm $TRYTONPASSFILE
@@ -20,4 +20,4 @@ if [ $1 == 'first-run' ]; then
 fi
 
 echo "Launching Trytond Server..."
-exec /usr/local/bin/trytond -c $TRYTOND_CONFIG -d $DATABASE_NAME -v
+exec trytond -c $TRYTOND_CONFIG -d $DATABASE_NAME -v
