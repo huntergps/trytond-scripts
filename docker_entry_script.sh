aebe9367b812c672ff6b520df22dccecd5370f43
Kulv3r <kulver@gmail.com>
2016-05-25 21:43:33 +0300

    docker entry script tests

--- docker_entry_script.sh
+++ docker_entry_script.sh
@@ -3,22 +3,21 @@
 echo "Generating config..."
 python /tryton/trytond-scripts/generate_config.py
 
+echo "First run."
+
+echo "Generating password file..."
+echo "$TRYTONPASS" > $TRYTONPASSFILE
+
+echo "Initializing the DB..."
+/usr/local/bin/trytond -c $TRYTOND_CONFIG -d $DATABASE_NAME -v --all
+
+echo "Removing password file..."
+rm $TRYTONPASSFILE
+
 if [ $1 == 'first-run' ]; then
-    echo "First run."
-
-    echo "Generating password file..."
-    echo "$TRYTONPASS" > $TRYTONPASSFILE
-
-    echo "Initializing the DB..."
-    /usr/local/bin/trytond -c $TRYTOND_CONFIG -d $DATABASE_NAME -v --all
-
-    echo "Removing password file..."
-    rm $TRYTONPASSFILE
-
     echo "Install ALL the modules..."
     python /tryton/trytond-scripts/install_modules.py
 fi
 
 echo "Launching Trytond Server..."
 exec /usr/local/bin/trytond -c $TRYTOND_CONFIG -d $DATABASE_NAME -v
-#exec bash
