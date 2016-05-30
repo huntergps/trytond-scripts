9bb79646e1732f04910124c2df6e6cd426454c96
Kulv3r <kulver@gmail.com>
2016-05-30 21:14:32 +0300

    install_modules prints

--- install_modules.py
+++ install_modules.py
@@ -9,8 +9,8 @@ CONFIG_FILE_PATH = '/tryton/config.ini'
 INSTALLED = []
 FAILED = []
 SKIPPED = []
-BLACKLIST = ["product_classification_taxonomic",
-             "purchase_request"]
+# BLACKLIST = ["product_classification_taxonomic",
+#              "purchase_request"]
 
 config = config.set_trytond(DB_NAME, config_file=CONFIG_FILE_PATH)
 
@@ -26,22 +26,27 @@ def main():
         if not_installed_module.state != u"installed":
             install_module(not_installed_module)
 
+    for array in (INSTALLED, SKIPPED, FAILED):
+        remove_doubles(array)
+
     print 'Done. \n' \
           'Installed modules: \n%s\n' \
-          'Skipped modules: \n%s\n' \
           'Failed modules: \n%s\n' % (
         '\n'.join(INSTALLED) or '-',
-        '\n'.join(SKIPPED) or '-',
         '\n'.join(FAILED) or '-'
     )
 
 
+def remove_doubles(array):
+    return list(set(array)).sort()
+
+
 def install_module(module):
     """Lookup uninstalled dependencies and recuresively install them."""
-    if module.name in BLACKLIST:
-        # print("[!] Skipped installation of blacklisted module {}".format(module.name))
-        SKIPPED.append(module.name)
-        return False
+    # if module.name in BLACKLIST:
+    #     print("[!] Skipped installation of blacklisted module {}".format(module.name))
+    #     SKIPPED.append(module.name)
+    #     return False
 
     # install missing dependencies recursively
     missing_dependencies = [m for m in module.parents if m.state != u"installed"]
@@ -55,13 +60,13 @@ def install_module(module):
             return False
 
     # mark module to install and run installation wizard
-    # print("Installing module {}".format(module.name))
+    print("Installing module {}".format(module.name))
     try:
         module.click("install")
         Wizard('ir.module.install_upgrade').execute('upgrade')
     except Exception as e:
-        # print("[!] Failed to install {}".format(module.name))
-        # print(e.message)
+        print("[!] Failed to install {}".format(module.name))
+        print(e.message)
         FAILED.append(module.name)
         return False
     else:
